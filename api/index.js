0e1ace53a371763015fc184504f5042d1bc2444f
JCPrime01 <vanzokiller@gmail.com>
2026-05-13 11:47:02 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=LMG1ZWV0ZSJ&afp8=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=QXT19M94E6J&afp8=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
