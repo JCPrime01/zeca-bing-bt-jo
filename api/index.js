a774320260b03d0f37184256d30d88bb13a921eb
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-28 10:50:01 -0300

    chore: update shareCode to WVLTY7LI0P8

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=B8MY150R3YC&afp8=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=WVLTY7LI0P8&afp8=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
