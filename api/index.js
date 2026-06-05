45f7a7afd05f118381a907c01901d4dfa37ddbe2
JCPrime01 <vanzokiller@gmail.com>
2026-06-05 12:04:17 -0300

    chore: shareCode HWKIZ4R08U1 -> M1VOMTBCIHI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=HWKIZ4R08U1&afp8=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=M1VOMTBCIHI&afp8=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
