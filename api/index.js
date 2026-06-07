222adfb6e42d6310def8a8eb66d532d242e6e8a8
JCPrime01 <vanzokiller@gmail.com>
2026-06-07 09:48:47 -0300

    chore: shareCode 9NWFDCM1CWL -> LRTVDVO0YOM (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=9NWFDCM1CWL&afp8=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=LRTVDVO0YOM&afp8=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
