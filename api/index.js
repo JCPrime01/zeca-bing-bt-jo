6a00cb5f7bc31dbdaab4c743981408bc02ab8e7b
JCPrime01 <vanzokiller@gmail.com>
2026-06-09 09:48:22 -0300

    chore: shareCode YKUUJFTOMC9 -> E5NYWVHJ9V1 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=YKUUJFTOMC9&afp8=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=E5NYWVHJ9V1&afp8=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
