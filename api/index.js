6d7ef3d76e20787591d0f48648120cf3a1a6ea69
JCPrime01 <vanzokiller@gmail.com>
2026-06-15 09:24:55 -0300

    chore: shareCode 7XV1PWZCMMZ -> FK5SVIUVDEV (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=7XV1PWZCMMZ&afp2=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=FK5SVIUVDEV&afp2=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
