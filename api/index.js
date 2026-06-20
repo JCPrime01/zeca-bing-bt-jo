4e0e1e251880eefd0cabb53227450bb320f4e7e2
JCPrime01 <vanzokiller@gmail.com>
2026-06-20 09:40:41 -0300

    chore: shareCode YEYWJXO3R6R -> 53XL1K5ENL7 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=YEYWJXO3R6R&afp2=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=53XL1K5ENL7&afp2=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
