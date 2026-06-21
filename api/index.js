208be85912087da9fa6be4885d9e4022490696b9
JCPrime01 <vanzokiller@gmail.com>
2026-06-21 11:00:36 -0300

    chore: shareCode 53XL1K5ENL7 -> 8J7FSNIPE75 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=53XL1K5ENL7&afp2=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=8J7FSNIPE75&afp2=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
