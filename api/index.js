caa22d828942c165bc03fda2b47c46e37fb0bc5e
JCPrime01 <vanzokiller@gmail.com>
2026-06-23 11:12:59 -0300

    chore: shareCode F3AHCFHPIRC -> Y35TYUEZDD9 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=F3AHCFHPIRC&afp2=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=Y35TYUEZDD9&afp2=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
