563c795a209e7400254462dd7c244b252119b456
JCPrime01 <vanzokiller@gmail.com>
2026-07-01 10:22:01 -0300

    chore: shareCode JA422LTMYBD -> VNU3ZXP29I7 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=JA422LTMYBD&afp2=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=VNU3ZXP29I7&afp2=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
