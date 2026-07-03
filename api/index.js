cecbb6e3a55d31543025e142ed8e6a1a0f0754c2
JCPrime01 <vanzokiller@gmail.com>
2026-07-03 09:32:43 -0300

    chore: shareCode 6NPNXEUJKJU -> V82HLZRCD6F (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=6NPNXEUJKJU&afp2=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=V82HLZRCD6F&afp2=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
