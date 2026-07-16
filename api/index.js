1f1b31e4cd4a9eea6da342f91be596837474c306
JCPrime01 <vanzokiller@gmail.com>
2026-07-16 09:51:38 -0300

    chore: shareCode MX0QITRCTFC -> 05NY0P58E2S (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=MX0QITRCTFC&afp2=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=05NY0P58E2S&afp2=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
